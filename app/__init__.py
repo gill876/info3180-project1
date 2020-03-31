c4f3ddcb033793828e411a1ed738249e1967df5a
Cargill Seiveright <microcargill@hotmail.com>
2020-03-31 04:44:51 -0500

    change to Heroku postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '?.jDTL_ge}PeG{v>ecXeG+64(eLc$D2c53Ku)w'
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/profile_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nqiqdzogsmdwcs:ab64936bcf863c3fd2e807627bc969953c254dcc1655adf8d276c2e0d930e704@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d21tjodb03ie7g'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
